b7b235fb269ce7205d7e8169d777ff0786103eff
michaelpacer <michaelpacer@gmail.com>
2016-11-28 18:26:15 -0800

    add regex to dependencies

--- setup.py
+++ setup.py
@@ -195,6 +195,7 @@ install_requires = setuptools_args['install_requires'] = [
     'bleach',
     'pandocfilters>=1.4.1',
     'testpath', 
+    'regex',
 ]
 
 extras_require = setuptools_args['extras_require'] = {
